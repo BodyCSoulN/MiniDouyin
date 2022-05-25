ae3747e3d88c15e9906d8930cd5a757e17c463ca
ElieenAndBella <1620055758@qq.com>
2022-05-25 17:52:27 +0800

    Frame

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Static("/static", "./public")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
