5fdbbd334b7b0cea416923549b610429287529b6
lxuegod <lxuegod@163.com>
2022-06-05 21:03:12 +0800

    Update

--- storage/table.go
+++ storage/table.go
@@ -1,16 +1,14 @@
 package storage
 
-import "gorm.io/gorm"
-
 // 用户表(作者)
-type User struct {
-	ID            int64
-	Name          string
-	FollowCount   int64
-	FollowerCount int64
-	IsFollow      bool `json:"is_follow,omitempty"`
-	gorm.Model
-}
+//type User struct {
+//	ID            int64
+//	Name          string
+//	FollowCount   int64
+//	FollowerCount int64
+//	IsFollow      bool `json:"is_follow,omitempty"`
+//	gorm.Model
+//}
 
 // DBUser 数据库用户表
 type DBUser struct {
@@ -24,30 +22,30 @@ type DBUser struct {
 }
 
 // 视频表
-type Video struct {
-	ID            int64
-	AuthorID      int64 `json:"author_id,omitempty"`
-	Author        User  `gorm:"foreignKey:AuthorID;references:ID;"`
-	PlayUrl       string
-	CoverUrl      string
-	FavoriteCount int64
-	CommentCount  int64
-	Title         string
-	IsFavorite    bool   `json:"is_favorite,omitempty"`
-	gorm.Model
-}
+//type Video struct {
+//	ID            int64
+//	AuthorID      int64 `json:"author_id,omitempty"`
+//	Author        User  `gorm:"foreignKey:AuthorID;references:ID;"`
+//	PlayUrl       string
+//	CoverUrl      string
+//	FavoriteCount int64
+//	CommentCount  int64
+//	Title         string
+//	IsFavorite    bool   `json:"is_favorite,omitempty"`
+//	gorm.Model
+//}
 
 // 评论表
-type Comment struct {
-	ID            int64
-	UserID        int64
-	User          User `gorm:"foreignKey:UserID;references:ID;"`
-	VideoID       int64
-	Video         Video `gorm:"foreignKey:VideoID;references:ID"`
-	Content       string
-	FavoriteCount int64
-	gorm.Model
-}
+//type Comment struct {
+//	ID            int64
+//	UserID        int64
+//	User          User `gorm:"foreignKey:UserID;references:ID;"`
+//	VideoID       int64
+//	Video         Video `gorm:"foreignKey:VideoID;references:ID"`
+//	Content       string
+//	FavoriteCount int64
+//	gorm.Model
+//}
 
 // 关注表
 type Attention struct {
