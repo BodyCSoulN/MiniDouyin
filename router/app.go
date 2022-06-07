102e5a611ef75325118952995390f746f714d60c
lxuegod <lxuegod@163.com>
2022-06-07 20:19:10 +0800

    增加comment路由

--- router/app.go
+++ router/app.go
@@ -15,6 +15,13 @@ func Router() *gin.Engine {
 	douyinRouter.POST("/publish/action", controller.PublishVideo)
 	douyinRouter.GET("/publish/list", controller.GetPublishList)
 
+	douyinRouter.GET("/comment/comment", controller.GetComment)
+	douyinRouter.GET("/comment/count", controller.GetCommentFavoriteCount)
+	douyinRouter.GET("/comment/front_list", controller.GetCommentListFront)
+	douyinRouter.GET("/comment/list", controller.GetCommentList)
+	douyinRouter.POST("/comment/delete", controller.DeleteComment)
+	douyinRouter.POST("/comment/add", controller.AddComment)
+
 	return r
 
 }
